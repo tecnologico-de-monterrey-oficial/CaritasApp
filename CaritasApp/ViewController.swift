26b175d4cd0879a1f1812f69ddc07cc517f6238c
ErnestoLV <ernestolopezvv@outlook.com>
2021-09-09 18:14:33 -0600

    Initial Commit

---
+++ CaritasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaritasApp
+//
+//  Created by Ernesto López on 09/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
