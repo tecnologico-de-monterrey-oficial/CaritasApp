880061c098fe6e0d3179d8f6760c67fa084093fa
ErickSolisMahl <A00904052@itesm.mx>
2021-09-17 12:00:09 -0700

    IDS-14 add Calendar

--- CaritasApp/ViewController.swift
+++ CaritasApp/ViewController.swift
@@ -6,13 +6,27 @@
 //
 //Cambio prueba dentro de main para el rebase
 
+import FSCalendar
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, FSCalendarDelegate {
+    
+    @IBOutlet var calendar: FSCalendar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        calendar.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE MM-dd-YYYY"
+        let string = formatter.string(from: date)
+        print(string)
+    
     }
 
 
